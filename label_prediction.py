e6b48a3fc6c2da39f155b3f9abaa94795d18456d
Cheng Ge <55502012+gc-js@users.noreply.github.com>
2025-04-10 16:59:23 +0800

    Add files via upload

---
+++ label_prediction.py
@@ -0,0 +1,73 @@
+import torch
+from creopep.utils import create_vocab, setup_seed
+from creopep.dataset_mlm import  get_paded_token_idx_gen, add_tokens_to_vocab
+import argparse
+import pandas as pd
+setup_seed(4)
+device = torch.device("cuda:0")
+parser = argparse.ArgumentParser('Label Prediction', add_help=False)
+
+def CreoPep(ctxs, X1, X2, model):
+    with open(ctxs, 'r') as f:
+        lines = f.readlines()
+    Seq_all  = []
+    subtype_all = []
+    potency_all  = []
+    topk_all = []
+    save_path = model
+    model = torch.load(save_path, weights_only=False)
+    model = model.to(device)
+
+    for X3 in lines:
+        vocab_mlm = create_vocab()
+        vocab_mlm = add_tokens_to_vocab(vocab_mlm)
+        predicted_token_probability_all = []
+        topk = []
+        new_seq = None
+        seq = [f"{X1}|{X2}|{X3}|||"]
+        vocab_mlm.token_to_idx["X"] = 4
+        padded_seq, _, idx_msa, _ = get_paded_token_idx_gen(vocab_mlm, seq, new_seq)
+        idx_msa = torch.tensor(idx_msa).unsqueeze(0).to(device)
+        mask_positions = [i for i, token in enumerate(padded_seq) if token == "X"]
+        if not mask_positions:
+            raise ValueError("Nothing found in the sequence to predict.")
+        for mask_position in mask_positions:
+            padded_seq[mask_position] = "[MASK]"
+            input_ids = vocab_mlm.__getitem__(padded_seq)
+            input_ids = torch.tensor([input_ids]).to(device)
+            logits = model(input_ids, idx_msa)
+            mask_logits = logits[0, mask_position, :]
+            predicted_token_probability, predicted_token_id = torch.topk((torch.softmax(mask_logits, dim=-1)), k=5)
+            topk.append(predicted_token_id)
+            predicted_token = vocab_mlm.idx_to_token[predicted_token_id[0].item()]
+            predicted_token_probability_all.append(predicted_token_probability[0].item())
+            padded_seq[mask_position] = predicted_token
+
+        cls_pos = vocab_mlm.to_tokens(list(topk[0]))
+        if X1 != "X":
+            Topk = X1
+            Subtype = X1
+            Potency = padded_seq[2],predicted_token_probability_all[0]
+        elif X2 != "X":
+            Topk = cls_pos
+            Subtype = padded_seq[1],predicted_token_probability_all[0]
+            Potency = X2
+        else:
+            Topk = cls_pos
+            Subtype = padded_seq[1],predicted_token_probability_all[0]
+            Potency = padded_seq[2],predicted_token_probability_all[1]
+        Seq_all.append(X3.strip())
+        subtype_all.append(Subtype)
+        potency_all.append(Potency)
+        topk_all.append(Topk)
+        data = {'Sequence': Seq_all, 'Subtype': subtype_all, 'Potency': potency_all, 'Topk': topk_all}
+        df = pd.DataFrame(data)
+        df.to_csv('./test/result.csv', index=False, encoding='utf-8-sig')
+
+if __name__ == '__main__':
+    parser.add_argument('-i', '--ctxs', default='./test/ctx.txt', required=True, type=str, help='Conotoxins: conotoxins need to be predicted.')
+    parser.add_argument('-s', '--subtype', default='X', type=str, help='Subtype: X if needs to be predicted.')
+    parser.add_argument('-p', '--potency', default='X', type=str, help='Potency: X if needs to be predicted.')
+    parser.add_argument('-m', '--model', default='./models/model_final.pt', type=str, help='Model: model parameters trained at different stages of data augmentation.')
+    args = parser.parse_args()
+    CreoPep(args.ctxs, args.subtype, args.potency,  args.model)
