852e3a806edd066ccfbe339b2a1097240a626878
Cheng Ge <55502012+gc-js@users.noreply.github.com>
2025-04-17 00:02:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="creopep",
     version="0.1",
-    package_dir={"":"creopep"}
+    package_dir={"": "creopep"},
+    packages=find_packages(where="creopep"),
+    install_requires=requirements,
+    python_requires='>=3.10',
+    setup_requires=['wheel'],
 )
