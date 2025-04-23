f358e9077cd72fbc403fe1656ebdd98655956464
ZhenQiang Zhang <3304828046@qq.com>
2025-04-23 19:58:25 +0800

    dataset

--- creopep/dataset_mlm.py
+++ creopep/dataset_mlm.py
@@ -4,8 +4,9 @@ import numpy as np
 import torch
 from torch.utils.data import TensorDataset, DataLoader
 from sklearn.model_selection import train_test_split
-from vocab import PepVocab
-from utils import mask, create_vocab
+
+from creopep.vocab import PepVocab
+from creopep.utils import mask, create_vocab
 
 addtition_tokens = ['<K16>', '<α1β1γδ>', '<Ca22>', '<AChBP>', '<K13>', '<α1BAR>', '<α1β1ε>', '<α1AAR>', '<GluN3A>', '<α4β2>',
                      '<GluN2B>', '<α75HT3>', '<Na14>', '<α7>', '<GluN2C>', '<NET>', '<NavBh>', '<α6β3β4>', '<Na11>', '<Ca13>', 
@@ -31,6 +32,7 @@ def split_seq(seq, vocab, get_seq=False):
     if get_seq == True:
         add = lambda x: [start] + [cls_label] + [act_label] + x + [end]
         pep_seq = seq.split('|')[2]
+        
         # return [start] + [cls_label] + [act_label] + vocab.split_seq(pep_seq) + [end]
         return add(vocab.split_seq(pep_seq))
     
@@ -40,7 +42,6 @@ def split_seq(seq, vocab, get_seq=False):
         msa2_seq = seq.split('|')[4]
         msa3_seq = seq.split('|')[5]
 
-        # return [vocab.split_seq(msa1_seq)]  + [vocab.split_seq(msa2_seq)]  + [vocab.split_seq(msa3_seq)]
         return [add(vocab.split_seq(msa1_seq))]  + [add(vocab.split_seq(msa2_seq))]  + [add(vocab.split_seq(msa3_seq))]
 
 def get_paded_token_idx(vocab_mlm, train_data_path):
@@ -57,30 +58,11 @@ def get_paded_token_idx(vocab_mlm, train_data_path):
     vocab_mlm.set_get_attn(is_get=False)
     padded_msa = vocab_mlm.truncate_pad(splited_msa, num_steps=54, padding_token='[PAD]')
     
-    idx_seq = vocab_mlm.__getitem__(padded_seq)
+    idx_seq = vocab_mlm[padded_seq]
     
-    idx_msa = vocab_mlm.__getitem__(padded_msa)
+    idx_msa = vocab_mlm[padded_msa]
 
     return padded_seq, idx_seq, idx_msa, attn_idx
-
-# def get_paded_token_idx_gen(vocab_mlm, seq):
-    
-#     splited_seq = split_seq(seq[0], vocab_mlm, True)
-#     splited_msa = split_seq(seq[0], vocab_mlm, False)
-    
-#     vocab_mlm.set_get_attn(is_get=True)
-#     padded_seq = vocab_mlm.truncate_pad(splited_seq, num_steps=54, padding_token='[PAD]')
-#     attn_idx = vocab_mlm.get_attention_mask_mat()
-
-#     vocab_mlm.set_get_attn(is_get=False)
-#     padded_msa = vocab_mlm.truncate_pad(splited_msa, num_steps=54, padding_token='[PAD]')
-    
-#     idx_seq = vocab_mlm.__getitem__(padded_seq)
-    
-#     idx_msa = vocab_mlm.__getitem__(padded_msa)
-
-#     return padded_seq, idx_seq, idx_msa, attn_idx
-
 
 def get_paded_token_idx_gen(vocab_mlm, seq, new_seq):
     if new_seq == None:
@@ -94,8 +76,9 @@ def get_paded_token_idx_gen(vocab_mlm, seq, new_seq):
 
         padded_msa = vocab_mlm.truncate_pad(splited_msa, num_steps=54, padding_token='[PAD]')
         
-        idx_seq = vocab_mlm.__getitem__(padded_seq)
-        idx_msa = vocab_mlm.__getitem__(padded_msa)
+        idx_seq = vocab_mlm[padded_seq]
+        idx_msa = vocab_mlm[padded_msa]
+        
     else:
         splited_seq = split_seq(seq[0], vocab_mlm, True)
         splited_msa = split_seq(seq[0], vocab_mlm, False)
@@ -104,14 +87,16 @@ def get_paded_token_idx_gen(vocab_mlm, seq, new_seq):
         attn_idx = vocab_mlm.get_attention_mask_mat()
         vocab_mlm.set_get_attn(is_get=False)
         padded_msa = vocab_mlm.truncate_pad(splited_msa, num_steps=54, padding_token='[PAD]')
-        idx_msa = vocab_mlm.__getitem__(padded_msa)
-        idx_seq = vocab_mlm.__getitem__(new_seq)
+        idx_msa = vocab_mlm[padded_msa]
+        idx_seq = vocab_mlm[new_seq]
+        
     return padded_seq, idx_seq, idx_msa, attn_idx
 
 def make_mask(seq_ser, start, end, time, vocab_mlm, labels, idx_msa, attn_idx, test_size, batch_size, seed, device):
+    
     seq_ser = pd.Series(seq_ser)
     masked_seq = seq_ser.apply(mask, args=(start, end, time))
-    masked_idx = vocab_mlm.__getitem__(list(masked_seq))
+    masked_idx = vocab_mlm[list(masked_seq)]
     masked_idx = torch.tensor(masked_idx)
     
     data_arrays = (masked_idx.to(device), labels.to(device), idx_msa.to(device), attn_idx.to(device)) 
@@ -121,21 +106,3 @@ def make_mask(seq_ser, start, end, time, vocab_mlm, labels, idx_msa, attn_idx, t
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
     
     return train_loader, test_loader
-
-if __name__ == '__main__':
-    vocab_mlm = create_vocab()
-    vocab_mlm = add_tokens_to_vocab(vocab_mlm)
-    padded_seq, idx_seq, idx_msa, attn_idx = get_paded_token_idx(vocab_mlm)
-    labels = torch.tensor(idx_seq)
-    idx_msa = torch.tensor(idx_msa)
-    attn_idx = torch.tensor(attn_idx)
-    for t in np.arange(1, 50):
-        padded_seq_copy = deepcopy(padded_seq)        
-        train_loader, test_loader = make_mask(padded_seq_copy, start=0, end=49, time=t, 
-                                              vocab_mlm=vocab_mlm, labels=labels, idx_msa=idx_msa, attn_idx=attn_idx)
-        for i, (masked_idx, label, msa, attn) in enumerate(train_loader):
-            print(f"the {i}th batch is that masked_idx is {masked_idx.shape}, labels is {label.shape}, idx_msa is {msa.shape}")
-        print(f"the {t}th time step is done")
-        
-        
-    
