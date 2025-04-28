dbd32928ec7cde004703a08c3dcfcf3946e00981
Cheng Ge <55502012+gc-js@users.noreply.github.com>
2025-04-29 01:09:10 +0800

    Update run.sh

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+BASE_DIR="foldx"
+PYTHON_SCRIPT="Interaction_Energy.py"
+TASK_NAME="a9a10"
+
+
+CASES=($(ls -d $BASE_DIR/*/))
+
+for case_path in "${CASES[@]}"; do
+    case_name=$(basename "$case_path")
+    echo "Processing case: $case_name" 
+    /home/ubuntu/anaconda3/envs/gecheng/bin/python "$PYTHON_SCRIPT" \
+        --path "$case_path" \
+        --task "$TASK_NAME" \
+        --num "$case_name"
+done
