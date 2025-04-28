5a9d7b3519b2842f5147ad24da16300a6b02883d
Cheng Ge <55502012+gc-js@users.noreply.github.com>
2025-04-28 23:00:16 +0800

    Create cif2pdb.py

---
+++ analysis/cif2pdb.py
@@ -0,0 +1,18 @@
+from Bio.PDB import MMCIFParser, PDBIO
+import os
+
+dir_path = r"./cif/"
+files = os.listdir(dir_path)
+
+for file in files:
+    if file.split(".")[1] == "cif":
+        pdb_name = file.split(".")[0]
+        input_cif_filename = os.path.join(dir_path, file)
+        output_pdb_filename = os.path.join(dir_path, pdb_name + ".pdb")
+
+        parser = MMCIFParser()
+        structure = parser.get_structure("Protein", input_cif_filename)
+
+        io = PDBIO()
+        io.set_structure(structure)
+        io.save(output_pdb_filename)
