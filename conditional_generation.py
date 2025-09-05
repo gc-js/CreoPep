91bddec5270176999534e50176965f07b74598b3
Cheng Ge <55502012+gc-js@users.noreply.github.com>
2025-09-05 13:35:44 +0800

    Update conditional_generation.py

--- conditional_generation.py
+++ conditional_generation.py
@@ -134,7 +134,7 @@ def CreoPep(X1, X2, τ, g_num, start, end, model_name, seed, output):
 if __name__ == '__main__':
     parser.add_argument('-is', '--subtype', default='<α7>', type=str, help='subtype of action. For example, α7.')
     parser.add_argument('-ip', '--potency', default='<high>', type=str, help='required potency. For example, High.')   
-    parser.add_argument('-t', '--temperature', default='1', type=int, help='τ: temperature factor controls the diversity of conotoxins generated. The higher the value, the higher the diversity.')
+    parser.add_argument('-t', '--temperature', default='1', type=float, help='τ: temperature factor controls the diversity of conotoxins generated. The higher the value, the higher the diversity.')
     parser.add_argument('-n', '--num', default='10', type=int, help='Number of generations')
     parser.add_argument('-b', '--begin', default='12', type=int, help='Min length for generating peptides')
     parser.add_argument('-e', '--end', default='16', type=int, help='Max length for generating peptides')
@@ -144,3 +144,4 @@ if __name__ == '__main__':
     parser.add_argument('--vocab', default='./data/vocab.txt', help='Vocab path')
     args = parser.parse_args()
     CreoPep(args.subtype, args.potency, args.temperature, args.num, args.begin, args.end, args.model, args.seed, args.output)
+
